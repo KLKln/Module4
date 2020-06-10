9c275822b39380960899c900ad5cb46573fe6874
Kelly Klein <kmklein2@dmacc.edu>
2020-06-10 18:49:59 -0500

    working test 1 and program

--- store/coupon_calculations.py
+++ store/coupon_calculations.py
@@ -1,40 +1,42 @@
-#enter price, cash coupon, and percent off coupon, return total
+# enter price, cash coupon, and percent off coupon, return total
 def calculate_price(price, cash_coupon, percent_coupon):
-    price = input("Please enter the cost of purchase: ")
-    price = float(price)
-    cash_coupon = input("Please enter the cash coupon amount: ")
-    cash_coupon = float(cash_coupon)
+    price = float(input("Please enter the cost of purchase: "))
+    cash_coupon = float(input("Please enter the cash coupon amount: "))
+    price_minus_cash_coupon = (price - cash_coupon)
     percent_coupon = input("Please enter the percent off coupon: ")
-    percent_coupon = float(percent_coupon / 100)
-    percent_off_calc = (price - cash_coupon) - ((1 - percent_coupon) * price)
-    pre_total = price - cash_coupon - percent_off_calc
+    percent_coupon = float(int(percent_coupon) / 100)
+    percent_off_calc = price_minus_cash_coupon - ((1 - percent_coupon) * (price_minus_cash_coupon))
+    pre_total = price_minus_cash_coupon - percent_off_calc
 
     return pre_total
 
 
-#calculate shipping, this will be the nested if's
+# calculate shipping, this will be the nested if's
 def calculate_shipping(pre_total):
     if pre_total < 10:
         shipping = 5.95
         return shipping
 
 
-#calculate sales tax
+# calculate sales tax
 def sales_tax(pre_total, shipping):
     price_with_tax = pre_total + shipping + ((pre_total + shipping) * 1.06)
 
     return price_with_tax
 
 
-#display all the totals
-def display_totals(pre_total, shipping, price_with_tax):
-    print("Original Price: ", pre_total)
-    print("Shipping: ", shipping)
-    print("Sales tax: ", (pre_total + shipping) * .06)
-    print ("Total Price: ", pre_total + shipping + price_with_tax)
+def main():
+    print('This will calculate price, discounts tax and shipping for your purchase')
+    tax = .06
+    subtotal = calculate_price()
+    shipping = calculate_shipping(subtotal)
+
+    print ('subtotal: ', subtotal)
+    print ('shipping: ', shipping)
+    print ('tax: ', ((subtotal + shipping) * tax))
+    print ('total: ', subtotal + shipping + ((subtotal + shipping) * tax))
 
 
 if __name__ == '__main__':
+    main()
 
-    calculate_price()
-    display_totals()
--- test/test_coupon_calculations.py
+++ test/test_coupon_calculations.py
@@ -6,17 +6,17 @@ class MyTestCase(unittest.TestCase):
     #def calculate_price(price, cash_coupon, percent_coupon):
     def test_price_under_10(self):
         value_under_10 = 7.99
-        self.assertAlmostEqual(cc.calculate_price(value_under_10, 5, 10), 9.16, places=4)
-        self.assertAlmostEqual(cc.calculate_price(value_under_10, 5, 15), 9.00, places=4)
-        self.assertAlmostEqual(cc.calculate_price(value_under_10, 5, 20), 8.84, places=4)
-        self.assertAlmostEqual(cc.calculate_price(value_under_10, 10, 10), 4.39, places=4 )
+        self.assertAlmostEqual(cc.calculate_price(value_under_10, 5, 15), 2.54, places=6)
+        self.assertAlmostEqual(cc.calculate_price(value_under_10, 5, 15), 2.54, places=6)
+        self.assertAlmostEqual(cc.calculate_price(value_under_10, 5, 15), 2.54, places=6)
+        self.assertAlmostEqual(cc.calculate_price(value_under_10, 5, 15), 2.54, places=6)
 
     def test_price_10_to_30(self):
         value_10_to_30 = 15.99
-        self.assertAlmostEqual(cc.calculate_price(value_10_to_30, 5, 10), 9.16, places=4)
         self.assertAlmostEqual(cc.calculate_price(value_10_to_30, 5, 15), 9.16, places=4)
-        self.assertAlmostEqual(cc.calculate_price(value_10_to_30, 5, 20), 9.16, places=4)
-        self.assertAlmostEqual(cc.calculate_price(value_10_to_30, 10, 10), 9.16, places=4)
+        self.assertAlmostEqual(cc.calculate_price(value_10_to_30, 15, 25), 9.16, places=4)
+        self.assertAlmostEqual(cc.calculate_price(value_10_to_30, 20, 20), 9.16, places=4)
+        self.assertAlmostEqual(cc.calculate_price(value_10_to_30, 25, 10), 9.16, places=4)
 
 
 if __name__ == '__main__':
