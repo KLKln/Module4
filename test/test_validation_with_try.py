2f7854f11ef83a98dcc5e8f9a89c69467784d380
Kelly Klein <kmklein2@dmacc.edu>
2020-06-11 19:42:15 -0500

    Update test_validation_with_try.py

--- test/test_validation_with_try.py
+++ test/test_validation_with_try.py
@@ -11,6 +11,11 @@ class MyTestCase(unittest.TestCase):
         with self.assertRaises(ValueError):
             average(-90, 89, 78)
 
+    def test_average_exception(self):
+        with self.assertRaises(ValueError):
+            average(90, -89, 78)
+
+
 
 if __name__ == '__main__':
     unittest.main()
